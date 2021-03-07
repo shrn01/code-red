6ba0f0fd078078e1f1b036ab04b2c500d2613d54
shrn01 <sharanjs1999@gmail.com>
2021-03-07 20:36:55 +0530

    working on sqlite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import sqlite3
+from data import *
 
 app = Flask(__name__)
 
@@ -10,6 +12,13 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/debug')
+def debug():
+    create_table()
+    add_movie(("Wall-E","2008",8.4,''))
+    s = str(get_all_movies())
+    return s
 
 if __name__ == "__main__":
     app.run(debug = True)
