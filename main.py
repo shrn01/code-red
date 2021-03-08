8d44f797cab5d7357d3204dea36a0d972bc3dfda
shrn01 <sharanjs1999@gmail.com>
2021-03-08 16:50:00 +0530

    added contributions page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import sqlite3
 from data import *
+from flask import request
 
 app = Flask(__name__)
 
@@ -8,6 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html",movies = get_all_movies())
 
+@app.route("/contribute", methods = ['GET', 'POST'])
+def contribute():
+    if request.method == "GET":
+        return render_template("contribute.html", post = False)
+    elif request.method == "POST":
+        print(request.form)
+        return render_template("contribute.html", post = True)
 
 @app.route('/about')
 def about():
@@ -16,14 +24,14 @@ def about():
 @app.route('/debug')
 def debug():
     create_table()
-    add_movie(("Wall-E","2008",8.4,'images/Wall-E.jpg'))
-    add_movie(("Deadpool","2016",8,'images/Deadpool.jpg'))
-    add_movie(("John Wick","2014",7.4,'images/John Wick.jpg'))
-    add_movie(("Alita: Battle Angel","2019",7.3,'images/Alita Battle Angel.jpg'))
-    add_movie(("The Dark Knight","2008",9,'images/The Dark Knight.jpg'))
-    add_movie(("Iron Man","2008",7.9,'images/Iron Man.jpg'))
-    add_movie(("Joker","2019",8.4,'images/Joker.jpg'))
-    add_movie(("Parasite","2019",8.6,'images/Parasite.jpg'))
+    add_movie(("Wall-E","2008",8.4,'static/images/Wall-E.jpg'))
+    add_movie(("Deadpool","2016",8,'static/images/Deadpool.jpg'))
+    add_movie(("John Wick","2014",7.4,'static/images/John Wick.jpg'))
+    add_movie(("Alita: Battle Angel","2019",7.3,'static/images/Alita Battle Angel.jpg'))
+    add_movie(("The Dark Knight","2008",9,'static/images/The Dark Knight.jpg'))
+    add_movie(("Iron Man","2008",7.9,'static/images/Iron Man.jpg'))
+    add_movie(("Joker","2019",8.4,'static/images/Joker.jpg'))
+    add_movie(("Parasite","2019",8.6,'static/images/Parasite.jpg'))
     s = str(get_all_movies())
     return s
 
